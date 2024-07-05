c46a031cbde364396ced5f026800842a0348133c
Gleb Ischenko <33964247+DilerFeed@users.noreply.github.com>
2024-07-05 10:00:14 +0300

    Update ArduinoStormworks_4segment.ino

--- arduino_scripts/ArduinoStormworks_4segment/ArduinoStormworks_4segment.ino
+++ arduino_scripts/ArduinoStormworks_4segment/ArduinoStormworks_4segment.ino
@@ -1,14 +1,14 @@
 #include "SevSeg.h"
 SevSeg sevseg;
 
-// Переменные для хранения состояний
+// Variables for storing states
 String command = "";
 bool newCommand = false;
 
 void setup() {
   Serial.begin(9600);
 
-  // Настройки для 4-значного сегментного дисплея
+  // Settings for 4-digit segment display
   byte numDigits = 4;
   byte digitPins[] = {10, 11, 12, 13};
   byte segmentPins[] = {9, 2, 3, 5, 6, 8, 7, 4};
@@ -21,7 +21,7 @@ void setup() {
 }
 
 void loop() {
-  // Чтение данных из последовательного порта
+  // Reading data from a serial port
   while (Serial.available() > 0) {
     char inChar = (char)Serial.read();
     if (inChar == '\n') {
@@ -42,7 +42,7 @@ void loop() {
 }
 
 void processCommand(String command) {
-  // Пример команды: "4SEGMENT1:1234" или "4SEGMENT1:12.34"
+  // Example command: "4SEGMENT1:1234" or "4SEGMENT1:12.34"
   if (command.startsWith("4SEGMENT1:")) {
     String number = command.substring(10);
     int decimalPosition = number.indexOf('.');
